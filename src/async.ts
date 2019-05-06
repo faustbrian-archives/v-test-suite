f17e9d68279e176061099eb01e22ecfebf2fe5d6
Brian Faust <hello@basecode.sh>
2019-05-06 07:19:47 +0300

    sync and async tests

---
+++ src/async.ts
@@ -0,0 +1,43 @@
+import { IValueStoreAsync } from "@faustbrian/common-value-store";
+import "jest-extended";
+
+export const complianceTestsAsync = <V>(store: IValueStoreAsync<V>, items: V[]): void => {
+	const itemsBool: boolean[] = new Array(5).fill(true);
+
+	beforeEach(() => store.flush());
+
+	it("should get all of the items in the store", async () => {
+		store.putMany(items);
+
+		await expect(store.all()).resolves.toEqual(items);
+	});
+
+	it("should put an item into the store", async () => {
+		await expect(store.put(items[0])).resolves.toBeTrue();
+	});
+
+	it("should put many items into the store", async () => {
+		await expect(store.putMany(items)).resolves.toEqual(itemsBool);
+	});
+
+	it("should be missing all items from the store", async () => {
+		for (const item of items) {
+			await expect(store.missing(item)).resolves.toBeTrue();
+		}
+	});
+
+	it("should remove an item from the store", async () => {
+		await expect(store.put(items[0])).resolves.toBeTrue();
+		await expect(store.forget(items[0])).resolves.toBeTrue();
+	});
+
+	it("should remove many items from the store", async () => {
+		await expect(store.putMany(items)).resolves.toEqual(itemsBool);
+		await expect(store.forgetMany(items)).resolves.toEqual(itemsBool);
+	});
+
+	it("should remove all items from the store", async () => {
+		await expect(store.putMany(items)).resolves.toEqual(itemsBool);
+		await expect(store.flush()).resolves.toBeTrue();
+	});
+};
---
+++ src/sync.ts
@@ -0,0 +1,43 @@
+import { IValueStoreSync } from "@faustbrian/common-value-store";
+import "jest-extended";
+
+export const complianceTestsSync = <V>(store: IValueStoreSync<V>, items: V[]): void => {
+	const itemsBool: boolean[] = new Array(5).fill(true);
+
+	beforeEach(() => store.flush());
+
+	it("should get all of the items in the store", () => {
+		store.putMany(items);
+
+		expect(store.all()).toEqual(items);
+	});
+
+	it("should put an item into the store", () => {
+		expect(store.put(items[0])).toBeTrue();
+	});
+
+	it("should put many items into the store", () => {
+		expect(store.putMany(items)).toEqual(itemsBool);
+	});
+
+	it("should be missing all items from the store", () => {
+		for (const item of items) {
+			expect(store.missing(item)).toBeTrue();
+		}
+	});
+
+	it("should remove an item from the store", () => {
+		expect(store.put(items[0])).toBeTrue();
+		expect(store.forget(items[0])).toBeTrue();
+	});
+
+	it("should remove many items from the store", () => {
+		expect(store.putMany(items)).toEqual(itemsBool);
+		expect(store.forgetMany(items)).toEqual(itemsBool);
+	});
+
+	it("should remove all items from the store", () => {
+		expect(store.putMany(items)).toEqual(itemsBool);
+		expect(store.flush()).toBeTrue();
+	});
+};
